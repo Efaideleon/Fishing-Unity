2081ddfe5c16865eec1210492e9b9329c18bd817
Efaideleon <eedeleon@uci.edu>
2023-08-05 16:19:05 -0700

    Revert "Merge pull request #16 from Efaideleon/Abel"
    This reverts commit 30427c1be647e010e4a4f49ce2588d9d14527ff6, reversing
    changes made to bbddd8fba064e38c6497cb2468a08801cf38295c.

--- Fishing-Game-Unity/Assets/Scripts/WaterPlaneMovement.cs
+++ Fishing-Game-Unity/Assets/Scripts/WaterPlaneMovement.cs
@@ -5,25 +5,27 @@ public class WaterPlaneMovement : MonoBehaviour
     public float speed = 0.13f;
     public float maxDistance = 0.2f;
 
-    private Vector3 startPosition;
-
+    private Material material;
     private void Start()
     {
-        startPosition = transform.position;
+        material = GetComponent<Renderer>().sharedMaterial;
     }
 
     private void Update()
     {
-        float newX = startPosition.x + Mathf.PingPong(Time.time * speed, maxDistance);
-        transform.position = new Vector3(newX, startPosition.y, startPosition.z);
-
-        float newY = startPosition.y + Mathf.PingPong(Time.time * speed, maxDistance);
-        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
     }
 
     public float GetWaterHeight()
     {
         return transform.position.y;
+    }
+    public float GetWaterMeshHeight(Vector3 position)
+    {
+        Vector3 positionOnMeshCoordinate = transform.InverseTransformPoint(position);
+        float sqrtValue = Mathf.Sqrt(Mathf.Pow(positionOnMeshCoordinate.x, 2) + Mathf.Pow(positionOnMeshCoordinate.z, 2));
+        float timesFrequency = sqrtValue * material.GetFloat("_Frequency");
+        float sinValue = Mathf.Sin(Time.time * material.GetFloat("_Speed") + timesFrequency);
+        return sinValue/2;
     }
 }
 
